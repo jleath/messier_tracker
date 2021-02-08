60ea85c329a502ba224c70e28bceb27a6d2ff9a3
Josh Leath <lawnfurniture@icloud.com>
2021-02-08 18:45:24 -0500

    first basic version. Basic functionality for logging messier observations present

--- messier-tracker.rb
+++ messier-tracker.rb
@@ -1,8 +1,149 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
+require 'yaml'
+
+def image_path
+  "/messier_imgs/"
+end
+
+before do
+  session[:logs] ||= {}
+end
+
+helpers do
+  def messier_data
+    YAML.load_file('data/messier_object_data.yml')
+  end
+
+  def log_options(messier_number, view)
+    case view
+    when :index then
+      <<~EOF
+      <li><a href="view/#{messier_number}">View logs</a></li>
+      <li><a href="/add/#{messier_number}">Add new log</a></li>
+      EOF
+    when :view then
+      <<~EOF
+      <li><a href="/add/#{messier_number}">Add new log</a></li>
+      EOF
+    when :add then
+      <<~EOF
+      <li><a href="/view/#{messier_number}">View logs</a></li>
+      EOF
+    end
+  end
+
+  def magnitude_color(magnitude)
+    case magnitude
+    when 0...3 then 'low_mag'
+    when 3...6 then 'midlow_mag'
+    when 6...9 then 'midhigh_mag'
+    when 9...12 then 'high_mag'
+    end
+  end
+
+  def messier_card(number, info, view)
+    title = info[:name].empty? ? number : "#{number} - #{info[:name]}"
+    log_entries = session[:logs][number]
+    num_log_entries = (log_entries ? log_entries.size : 0)
+    <<~EOF
+    <div class="messier_card #{'tracked' if num_log_entries > 0}">
+      <div class="info">
+        <h1>#{title}</h1>
+        <ul>
+          <li>Type: #{info[:type]}</li>
+          <li>Constellation: #{info[:constellation]}</li>
+          <li>Apparent Magnitude:
+            <span class="magnitude #{magnitude_color(info[:magnitude].to_f)}">#{info[:magnitude]}</span>
+          </li>
+        </ul>
+        <div class="log_options">
+          <ul>
+            <li>Logged #{num_log_entries} time#{'s' if num_log_entries != 1}</li>
+            #{log_options(number, view)}
+          </ul>
+        </div>
+      </div>
+      <img src="/messier_imgs/#{number}.jpg">
+    </div>
+    EOF
+  end
+end
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
 
 get '/' do
-  headers['Content-Type'] = 'text/html;charset=utf-8'
-  erb '<h1>Hello World</h1>'
+  erb :index
+end
+
+get '/view/:number' do
+  @number = params[:number]
+  @info = messier_data[@number]
+  session[:logs][@number] ||= []
+  @log_entries = session[:logs][@number]
+  erb :view_logs
+end
+
+get '/add/:number' do
+  @number = params[:number]
+  @info = messier_data[@number]
+  erb :add_new
+end
+
+post '/add/:number' do
+  @number = params[:number]
+  @info = messier_data[@number]
+  if params[:log_entry].nil? || params[:log_entry].empty?
+    session[:message] = "You cannot submit an empty log."
+    status 422
+    erb :add_new
+  else
+    session[:logs][@number] ||= []
+    session[:logs][@number].append(params[:log_entry])
+    session[:message] = "Entry added for #{@number}"
+    redirect "/view/#{@number}"
+  end
+end
+
+post '/edit/:number/log/:entry_index' do
+  @number = params[:number]
+  log_entry_index = params[:entry_index].to_i
+  new_entry = params[:new_entry]
+  @log_entries = session[:logs][@number] || []
+  @info = messier_data[@number]
+  # check for nonexistent entry
+  if @log_entries.empty?
+    session[:message] = "Log entry #{log_entry_index} for #{@number} does not exist."
+    status 422
+    erb :view_logs
+  # check for attempt to save unmodified log
+  elsif new_entry == @log_entries[log_entry_index]
+    session[:message] = "No changes made to log entry #{log_entry_index} for #{@number}."
+    status 422
+    erb :view_logs
+  else
+    @log_entries[log_entry_index] = new_entry
+    session[:message] = "Log entry #{log_entry_index} for #{@number} has been edited."
+    redirect "/view/#{@number}"
+  end
+end
+
+post '/delete/:number/log/:entry_index' do
+  @number = params[:number]
+  log_entry_index = params[:entry_index].to_i
+  @log_entries = session[:logs][@number] || []
+  if @log_entries[log_entry_index]
+    @log_entries.delete_at(log_entry_index)
+    session[:message] = "Log entry #{log_entry_index} for #{@number} has been deleted."
+    redirect "/view/#{@number}"
+  else
+    session[:message] = "Log entry #{log_entry_index} for #{@number} does not exist."
+    status 422
+    @info = messier_data[@number]
+    erb :view_logs
+  end
 end
---
+++ test/messier_tracker_test.rb
@@ -0,0 +1,143 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'rack/test'
+
+require_relative '../messier-tracker'
+
+Minitest::Reporters.use!
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def existing_logs(messier_number, num_logs)
+    logs = []
+    num_logs.times do |index|
+      logs.append("test#{index}")
+    end
+    { 'rack.session' => { logs: { messier_number => logs } } }
+  end
+
+  def session
+    last_request.env['rack.session']
+  end
+
+  def test_index
+    get '/'
+    assert_equal 200, last_response.status
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+    assert_includes last_response.body, 'M1'
+    assert_includes last_response.body, 'View logs'
+    assert_includes last_response.body, 'Add new log'
+    assert_equal 110, last_response.body.scan(/<a href=\"view\/.*?\">View logs<\/a>/).size
+  end
+
+  def test_view_empty_log
+    get '/view/M33'
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'M33'
+    assert_includes last_response.body, 'Logged 0 times'
+    assert_includes last_response.body, 'Add new log'
+    refute_includes last_response.body, 'View logs'
+  end
+
+  def test_view_log
+    get '/view/M33', {}, existing_logs("M33", 1)
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'test0'
+    assert_includes last_response.body, 'Logged 1 time'
+    refute_includes last_response.body, 'View logs'
+
+    get '/view/M33', {}, existing_logs("M33", 2)
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'test0'
+    assert_includes last_response.body, 'test1'
+    assert_includes last_response.body, 'Logged 2 times'
+  end
+
+  def test_add_log_form
+    get '/add/M33'
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'M33'
+    assert_includes last_response.body, 'Observation Log:'
+    assert_includes last_response.body, '<button type="submit">Save Log</button>'
+
+    get '/add/M33', {}, existing_logs("M33", 2)
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'Logged 2 times'
+    assert_includes last_response.body, 'View logs'
+    refute_includes last_response.body, 'Add new log'
+  end
+
+  def test_submit_empty_log
+    post '/add/M33', { 'log_entry' => '' }
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, 'You cannot submit an empty log.'
+    refute_includes last_response.body, 'messier_card tracked'
+
+    post '/add/M33', { 'log_entry' => nil }
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, 'You cannot submit an empty log.'
+    refute_includes last_response.body, 'messier_card tracked'
+  end
+
+  def test_submit_valid_log
+    post '/add/M33', { 'log_entry' => 'test' }
+    assert_equal 302, last_response.status
+    assert_equal 'Entry added for M33', session[:message]
+    assert_equal 'http://example.org/view/M33', last_response['Location']
+
+    get last_response['Location']
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'test'
+    assert_includes last_response.body, 'messier_card tracked'
+  end
+
+  def test_delete_log
+    post '/delete/M33/log/0', {}, existing_logs('M33', 1)
+    assert_equal 302, last_response.status
+    assert_equal 'http://example.org/view/M33', last_response['Location']
+
+    get last_response['Location']
+    assert_equal 200, last_response.status
+    refute_includes last_response.body, 'test'
+    assert_includes last_response.body, 'Logged 0 times'
+    refute_includes last_response.body, 'messier_card tracked'
+  end
+
+  def test_delete_nonexistent_log
+    post '/delete/M33/log/0'
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, 'Log entry 0 for M33 does not exist.'
+    assert_includes last_response.body, 'Logged 0 times'
+  end
+
+  def test_edit_log
+    post '/edit/M33/log/0', { 'new_entry' => 'modified' }, existing_logs('M33', 3)
+    assert_equal 302, last_response.status
+    assert_equal 'http://example.org/view/M33', last_response['Location']
+    assert_equal 'Log entry 0 for M33 has been edited.', session[:message]
+
+    get last_response['Location']
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'modified'
+    assert_includes last_response.body, 'Logged 3 times'
+  end
+
+  def test_edit_log_no_changes
+    post '/edit/M33/log/0', { 'new_entry' => 'test0' }, existing_logs('M33', 1)
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, 'No changes made to log entry 0 for M33'
+  end
+
+  def test_edit_nonexistent_log
+    post '/edit/M33/log/0', { 'new_entry' => 'test0' }
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, 'Log entry 0 for M33 does not exist.'
+  end
+end
